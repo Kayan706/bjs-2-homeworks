8d74fd13b9309ce8805e2276de7408f7a82e2909
Dmitriy Khraltsov <life608@yandex.ru>
2025-03-10 23:14:59 +0300

    home work

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,6 +1,20 @@
 function compareArrays(arr1, arr2) {
-  
-}
+    let a = 0;
+    if(arr1.length > arr2.length || arr1.length < arr2.length ){
+      return false
+    } else {
+      for(let i = 0; arr1.length > i; i++){
+        if (arr1[i] === arr2[i]){
+          a+=1
+        }
+      }
+    }
+    if (a === arr1.length){
+      return true
+    } else {
+      return false
+    }
+  }
 
 function getUsersNamesInAgeRange(users, gender) {
   
--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,51 @@
+
 function Student(name, gender, age) {
-  
+ this.name = name;
+ this.gender = gender;
+ this.age = age;
+ this.marks = []
 }
 
+
 Student.prototype.setSubject = function (subjectName) {
-  
+  this.subject = subjectName
 }
 
 Student.prototype.addMarks = function (...marks) {
-  
+  if (this.excluded != undefined){
+    return 0
+  }
+  if (this.marks === undefined) {
+      this.marks = [...marks]
+  } else {
+    this.marks.push(...marks)
+  }
+
 }
 
+
+
 Student.prototype.getAverage = function () {
+  if (this.marks === 0 || this.excluded !== undefined){
+    return 0
+  } else if (this.marks.length === 0){
+    return 0
+  }
+  let sum = 0;
+  this.marks.forEach(item => sum += item);
+  return sum / this.marks.length;
   
+
 }
 
 Student.prototype.exclude = function (reason) {
+
+  if (this.marks.length === 0) {
+    delete this.subject
+    delete this.marks
+    this.excluded = reason
+  }
   
+
 }
+
